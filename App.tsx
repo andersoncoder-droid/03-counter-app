0445367fed2662f0635426dbaad8679f932eac15
Anderson <andersonlosada.11@gmail.com>
2025-11-26 23:34:26 -0500

    Button floating style with long press reset functionality

--- App.tsx
+++ App.tsx
@@ -8,8 +8,12 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.textHuge}>{count}</Text>
-      <Pressable onPress={() => setCount(count + 1)}>
-        <Text>+1</Text>
+      <Pressable
+        style={styles.floatingButton}
+        onPress={() => setCount(count + 1)}
+        onLongPress={() => setCount(0)}
+      >
+        <Text style={{ color: 'white', fontSize: 20 }}>+1</Text>
       </Pressable>
       <StatusBar style='auto' />
     </View>
@@ -27,5 +31,22 @@ const styles = StyleSheet.create({
   textHuge: {
     fontSize: 120,
     fontWeight: '100',
+  },
+
+  floatingButton: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    backgroundColor: '#65558F',
+    padding: 20,
+    borderRadius: 15,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    elevation: 3,
+    shadowRadius: 4,
   },
 });
